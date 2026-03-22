aa036828941699b396c92158dc1df3c24e509a11
Saveliy Borisov <borisov.talim@yandex.ru>
2026-03-23 00:21:46 +0300

    Prefer GPU-capable SYCL queue by default

--- include/sycl_devices.h
+++ include/sycl_devices.h
@@ -2,6 +2,12 @@
 
 #include <CL/sycl.hpp>
 
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
 namespace FDTD_sycl {
 
     template <typename T>
@@ -10,18 +16,61 @@ namespace FDTD_sycl {
     class AllDevices
     {
     public:
-        AllDevices() : default_device{ sycl::default_selector{} },
-            cpu_device{ sycl::cpu_selector{}, sycl::async_handler{} }                
+        AllDevices() : default_device{ sycl::default_selector{}, sycl::async_handler{} },
+            cpu_device{ sycl::cpu_selector{}, sycl::async_handler{} },
+            gpu_device{ create_gpu_fallback_queue() },
+            active_device{ select_active_device() }
         {
-            try {
-                gpu_device = sycl::queue { sycl::gpu_selector{}, sycl::async_handler{} };
-            } catch (sycl::exception const& e) {
-                gpu_device = sycl::queue { sycl::cpu_selector{}, sycl::async_handler{} };
-            }
         };
+
         sycl::queue default_device;
         sycl::queue cpu_device;
         sycl::queue gpu_device;
+        sycl::queue active_device;
+
+    private:
+        static std::string normalized_device_preference() {
+            const char* value = std::getenv("FDTD_SYCL_DEVICE");
+            if (value == nullptr) {
+                return "auto";
+            }
+
+            std::string preference(value);
+            std::transform(preference.begin(), preference.end(), preference.begin(), [](unsigned char ch) {
+                return static_cast<char>(std::tolower(ch));
+            });
+            return preference;
+        }
+
+        static sycl::queue create_gpu_fallback_queue() {
+            try {
+                return sycl::queue { sycl::gpu_selector{}, sycl::async_handler{} };
+            } catch (const sycl::exception&) {
+                return sycl::queue { sycl::cpu_selector{}, sycl::async_handler{} };
+            }
+        }
+
+        sycl::queue select_active_device() const {
+            const std::string preference = normalized_device_preference();
+
+            if (preference == "cpu") {
+                return cpu_device;
+            }
+
+            if (preference == "default") {
+                return default_device;
+            }
+
+            if (preference == "gpu" && gpu_device.get_device().is_gpu()) {
+                return gpu_device;
+            }
+
+            if (gpu_device.get_device().is_gpu()) {
+                return gpu_device;
+            }
+
+            return cpu_device;
+        }
     };
 
     extern AllDevices node;
